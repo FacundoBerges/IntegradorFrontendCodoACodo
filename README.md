11c975aaf3c08b7e56f354e004932a75f072fe60
Facundo Berges <facundo.h.berges@gmail.com>
2023-05-28 19:25:06 -0300

    Adicionando JS a proyecto, integrador JS

---
+++ README.md
@@ -0,0 +1,7 @@
+# IntegradorFrontendCodoACodo
+
+Proyecto integrador de frontend del curso Fullstack Java en Codo a Codo
+
+## Deploy en github pages:
+
+https://facundoberges.github.io/IntegradorFrontendCodoACodo/
---
+++ js/tickets.js
@@ -0,0 +1,63 @@
+const PRICE = 200;
+const STUDENT_DISCOUNT = 80;
+const TRAINEE_DISCOUNT = 50;
+const JUNIOR_DISCOUNT = 15;
+
+document.getElementById("price").innerHTML = PRICE;
+document.getElementById("studentDiscount").innerHTML = STUDENT_DISCOUNT;
+document.getElementById("traineeDiscount").innerHTML = TRAINEE_DISCOUNT;
+document.getElementById("juniorDiscount").innerHTML = JUNIOR_DISCOUNT;
+
+const getDiscount = (category) => {
+    let discount;
+
+    switch (category) {
+        case "student":
+            discount = STUDENT_DISCOUNT;
+            break;
+        case "trainee":
+            discount = TRAINEE_DISCOUNT;
+            break;
+        case "junior":
+            discount = JUNIOR_DISCOUNT;
+            break;
+        default:
+            discount = 0;
+            break;
+    }
+
+    return discount / 100;
+};
+
+const getTicketDiscountPrice = (price, discount) => {
+    if(discount <= 0 || isNaN(discount)) {
+        return price;
+    }
+
+    return (price - (price * discount));
+};
+
+const calculatePrice = (price, quantity, discount) => {
+    if(isNaN(quantity) || quantity < 1){
+        return 0;
+    }
+
+    return (quantity * getTicketDiscountPrice(price, discount));
+};
+
+const getPrice = () => {
+    const category = document.getElementById("category").value;
+    const quantity = Math.round( parseInt( document.getElementById("quantity").value ) );
+    
+    const discount = getDiscount(category);
+    const totalPrice = calculatePrice(PRICE, quantity, discount);
+
+    return totalPrice;
+};
+
+document.getElementById("submitButton").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    totalPrice = getPrice();
+    document.getElementById("totalPrice").innerHTML = totalPrice;
+});
